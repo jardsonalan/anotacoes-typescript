69ade58c5d15cc165dfa8b0938724b3e5a874595
Jardson Alan <138258644+jardsonalan@users.noreply.github.com>
2024-10-13 21:35:08 -0300

    update: type inference; declarando o tipo da variável

---
+++ CFBCursos/Aula - 03/aula03.js
@@ -0,0 +1,8 @@
+"use strict";
+let nome; // Declarando o tipo da variável
+nome = 'Jardson';
+nome = 'Alan';
+let valor; // int, float, bin, hex
+valor = 10.5;
+console.log(valor);
+console.log(nome);
---
+++ CFBCursos/Aula - 03/aula03.ts
@@ -0,0 +1,9 @@
+let nome:string; // Declarando o tipo da variável
+nome = 'Jardson';
+nome = 'Alan';
+
+let valor:number; // int, float, bin, hex
+valor=10.5;
+
+console.log(valor);
+console.log(nome);
