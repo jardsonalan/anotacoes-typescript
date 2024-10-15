5827ee64754e7b0d49ff88d5ffd3ef17edc4f908
Jardson Alan <138258644+jardsonalan@users.noreply.github.com>
2024-10-15 18:37:30 -0300

    update: enum

---
+++ CFBCursos/Enum/aula08.js
@@ -0,0 +1,34 @@
+"use strict";
+var dias;
+(function (dias) {
+    dias[dias["domingo"] = 0] = "domingo";
+    dias[dias["segunda"] = 1] = "segunda";
+    dias[dias["terca"] = 2] = "terca";
+    dias[dias["quarta"] = 3] = "quarta";
+    dias[dias["quinta"] = 4] = "quinta";
+    dias[dias["sexta"] = 5] = "sexta";
+    dias[dias["sabado"] = 6] = "sabado";
+})(dias || (dias = {}));
+;
+const d = new Date();
+var cores;
+(function (cores) {
+    cores["branco"] = "#fff";
+    cores["preto"] = "#000";
+    cores["vermelho"] = "#f00";
+    cores["verde"] = "#0f0";
+    cores["azul"] = "#00f";
+})(cores || (cores = {}));
+;
+console.log(cores.branco);
+console.log(cores['preto']);
+var tipoUsuario;
+(function (tipoUsuario) {
+    tipoUsuario[tipoUsuario["USER"] = 0] = "USER";
+    tipoUsuario[tipoUsuario["ADMIN"] = 1] = "ADMIN";
+    tipoUsuario[tipoUsuario["SUPER"] = 2] = "SUPER";
+})(tipoUsuario || (tipoUsuario = {}));
+;
+console.log(tipoUsuario.SUPER);
+const tp = tipoUsuario.SUPER;
+console.log(tp);
