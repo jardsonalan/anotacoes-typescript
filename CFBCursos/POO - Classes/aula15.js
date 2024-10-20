4bc8a258badd383966468cfcabf1876ebc0f69d1
Jardson Alan <138258644+jardsonalan@users.noreply.github.com>
2024-10-19 21:29:27 -0300

    update: método construtor

--- CFBCursos/POO - Classes/aula15.js
+++ CFBCursos/POO - Classes/aula15.js
@@ -4,14 +4,18 @@ class Computador {
     ram;
     cpu;
     ligado;
+    constructor(nome, ram, cpu) {
+        this.nome = nome;
+        this.ram = ram;
+        this.cpu = cpu;
+        this.ligado = false;
+    }
+    ;
 }
 ;
-let computador1 = new Computador();
-let computador2 = new Computador();
-let computador3 = new Computador();
-computador1.nome = 'Rapidão';
-computador2.nome = 'Carão';
-computador3.nome = 'Gamer';
-console.log(computador1.nome);
-console.log(computador2.nome);
-console.log(computador3.nome);
+let computador1 = new Computador('Rapidão', 64, 10);
+let computador2 = new Computador('Carão', 32, 5);
+let computador3 = new Computador('Gamer', 128, 10);
+console.log(computador1.nome, computador1.ram, computador1.cpu, computador1.ligado);
+console.log(computador2.nome, computador2.ram, computador2.cpu, computador2.ligado);
+console.log(computador3.nome, computador3.ram, computador3.cpu, computador3.ligado);
--- CFBCursos/POO - Classes/aula15.ts
+++ CFBCursos/POO - Classes/aula15.ts
@@ -1,22 +1,30 @@
 // POO - Classes
 class Computador {
-    nome?:string;
-    ram?:number;
-    cpu?:number;
-    ligado?:boolean;
+    nome:string;
+    ram:number;
+    cpu:number;
+    ligado:boolean;
+
+    // Método construtor
+    constructor(nome:string, ram:number, cpu:number){
+        this.nome = nome; // this - se refere a Classe
+        this.ram = ram;
+        this.cpu = cpu;
+        this.ligado = false;
+    };
 };
 
 // Objetos da Classe
-let computador1 = new Computador();
-let computador2 = new Computador();
-let computador3 = new Computador();
+let computador1 = new Computador('Rapidão', 64, 10);
+let computador2 = new Computador('Carão', 32, 5);
+let computador3 = new Computador('Gamer', 128, 10);
 
 // Atribuições ao parâmetro (nome)
-computador1.nome = 'Rapidão';
-computador2.nome = 'Carão';
-computador3.nome = 'Gamer';
+// computador1.nome = 'Rapidão';
+// computador2.nome = 'Carão';
+// computador3.nome = 'Gamer';
 
 // Obtendo o valor do parâmetro (nome)
-console.log(computador1.nome);
-console.log(computador2.nome);
-console.log(computador3.nome);
+console.log(computador1.nome, computador1.ram, computador1.cpu, computador1.ligado);
+console.log(computador2.nome, computador2.ram, computador2.cpu, computador2.ligado);
+console.log(computador3.nome, computador3.ram, computador3.cpu, computador3.ligado);
