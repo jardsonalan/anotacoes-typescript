3975f1118db1055f49eb20998177cffdf3e04df4
Jardson Alan <138258644+jardsonalan@users.noreply.github.com>
2024-10-21 16:47:22 -0300

    update: herança - parte 02; super()

--- CFBCursos/POO - Herança/aula19.js
+++ CFBCursos/POO - Herança/aula19.js
@@ -2,20 +2,37 @@
 class Conta {
     numero;
     titular;
-    constructor(numero, titular) {
-        this.numero = numero;
+    constructor(titular) {
+        this.numero = this.gerarNumeroConta();
         this.titular = titular;
+    }
+    ;
+    gerarNumeroConta() {
+        let gerar = Math.floor(Math.random() * 100000) + 1;
+        return gerar;
     }
     ;
 }
 ;
 class ContaPF extends Conta {
+    cpf;
+    constructor(titular, cpf) {
+        super(titular);
+        this.cpf = cpf;
+    }
+    ;
 }
 ;
 class ContaPJ extends Conta {
+    cnpj;
+    constructor(titular, cnpj) {
+        super(titular);
+        this.cnpj = cnpj;
+    }
+    ;
 }
 ;
-let titular1 = new ContaPF(123, 'Jardson');
-let titular2 = new ContaPJ(456, 'Alan');
-console.log(titular1.titular);
-console.log(titular2.titular);
+let titular1 = new ContaPF('Jardson', 111);
+let titular2 = new ContaPJ('Alan', 222333);
+console.log(titular1.titular, titular1.numero);
+console.log(titular2.titular, titular2.numero);
--- CFBCursos/POO - Herança/aula19.ts
+++ CFBCursos/POO - Herança/aula19.ts
@@ -1,24 +1,39 @@
 class Conta {
-    private numero:number;
+    public numero:number;
     public titular:string;
     // Método construtor
-    constructor(numero:number, titular:string){
-        this.numero = numero;
+    constructor(titular:string){
+        this.numero = this.gerarNumeroConta(); // recebe o número gerado pela função (gerarNumeroConta)
         this.titular = titular;
+    };
+
+    gerarNumeroConta():number{
+        let gerar = Math.floor(Math.random()*100000)+1;
+        return gerar;
+    }; // gera um número aleatório
+};
+
+// Classe Filha (Conta Pessoa Física)
+class ContaPF extends Conta {
+    private cpf:number;
+    constructor(titular:string, cpf:number){
+        super(titular); // sempre se refere a classe pai
+        this.cpf = cpf;
     };
 };
 
-class ContaPF extends Conta {
-    
-};
-
+// Classe Filha (Conta Pessoa Jurídica)
 class ContaPJ extends Conta {
-    
+    private cnpj:number;
+    constructor(titular:string, cnpj:number){
+        super(titular);
+        this.cnpj = cnpj;
+    };
 };
 
 // Objeto da classe
-let titular1 = new ContaPF(123, 'Jardson');
-let titular2 = new ContaPJ(456, 'Alan');
+let titular1 = new ContaPF('Jardson', 111);
+let titular2 = new ContaPJ('Alan', 222333);
 
-console.log(titular1.titular);
-console.log(titular2.titular);
+console.log(titular1.titular, titular1.numero);
+console.log(titular2.titular, titular2.numero);
