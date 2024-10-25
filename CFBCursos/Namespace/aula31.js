757fa60f6340474bee664996b816754ea68dbcb1
Jardson Alan <138258644+jardsonalan@users.noreply.github.com>
2024-10-25 20:35:19 -0300

    update: namespace - parte 03; criação de novas classes; criação de novos métodos

--- CFBCursos/Namespace/aula31.js
+++ CFBCursos/Namespace/aula31.js
@@ -1,16 +1,68 @@
 "use strict";
 var Veiculos;
 (function (Veiculos) {
+    let Cores;
+    (function (Cores) {
+        Cores[Cores["Preto"] = 0] = "Preto";
+        Cores[Cores["Branco"] = 1] = "Branco";
+        Cores[Cores["Vermelho"] = 2] = "Vermelho";
+        Cores[Cores["Amarelo"] = 3] = "Amarelo";
+        Cores[Cores["Azul"] = 4] = "Azul";
+        Cores[Cores["Prata"] = 5] = "Prata";
+    })(Cores || (Cores = {}));
+    ;
     class Carro {
         nome;
         motor;
-        constructor(nome) {
+        cor;
+        constructor(nome, motor, cor) {
             this.nome = nome;
-            this.motor = new Motores.Motor(3, 100);
+            this.motor = motor;
+            this.cor = Cores[cor];
+        }
+        ;
+        ligar() {
+            this.motor.liga = true;
+        }
+        ;
+        desligar() {
+            this.motor.liga = false;
+        }
+        ;
+        get minhaCor() {
+            return this.cor;
+        }
+        ;
+        get meuNome() {
+            return this.nome;
+        }
+        ;
+        get estouLigado() {
+            return (this.motor.liga ? 'Sim' : 'Não');
+        }
+        ;
+        get minhaPotencia() {
+            return this.motor.potenciaMotor;
         }
         ;
     }
     Veiculos.Carro = Carro;
+    ;
+    class CarroEsportivo extends Carro {
+        constructor(nome, cor) {
+            super(nome, new Motores.Motor(6, 300, new Motores.Turbo(100)), cor);
+        }
+        ;
+    }
+    Veiculos.CarroEsportivo = CarroEsportivo;
+    ;
+    class CarroPopular extends Carro {
+        constructor(nome, cor) {
+            super(nome, new Motores.Motor(3, 100), cor);
+        }
+        ;
+    }
+    Veiculos.CarroPopular = CarroPopular;
     ;
 })(Veiculos || (Veiculos = {}));
 ;
@@ -27,6 +79,7 @@ var Motores;
         }
         ;
     }
+    Motores.Turbo = Turbo;
     ;
     class Motor {
         ligado;
@@ -55,3 +108,10 @@ var Motores;
     ;
 })(Motores || (Motores = {}));
 ;
+const carro1 = new Veiculos.CarroEsportivo('Ferrari', 5);
+const carro2 = new Veiculos.CarroPopular('Civic', 0);
+carro1.ligar();
+carro2.ligar();
+console.log(`Nome: ${carro1.meuNome} | Cor: ${carro1.minhaCor} | Potência: ${carro1.minhaPotencia} | Ligado: ${carro1.estouLigado}`);
+console.log('---------------------------------------------');
+console.log(`Nome: ${carro2.meuNome} | Cor: ${carro2.minhaCor} | Potência: ${carro2.minhaPotencia} | Ligado: ${carro2.estouLigado}`);
--- CFBCursos/Namespace/aula31.ts
+++ CFBCursos/Namespace/aula31.ts
@@ -1,13 +1,66 @@
 // Namespace
 namespace Veiculos {
+
+    enum Cores{
+        'Preto',
+        'Branco',
+        'Vermelho',
+        'Amarelo',
+        'Azul',
+        'Prata'
+    };
     // Utiliza o export para instânciar a classe fora do namespace
     export abstract class Carro {
-        public nome:string;
-        public motor:Motores.Motor;
+        private nome:string;
+        private motor:Motores.Motor;
+        private cor:String;
 
-        constructor(nome:string){
+        constructor(nome:string, motor:Motores.Motor, cor:Cores){
             this.nome = nome;
-            this.motor = new Motores.Motor(3, 100);
+            this.motor = motor;
+            this.cor = Cores[cor];
+        };
+
+        // Método para ligar o motor
+        public ligar(){
+            this.motor.liga = true;
+        };
+
+        // Método para desligar o motor
+        public desligar(){
+            this.motor.liga = false;
+        };
+
+        // Método para retornar a cor do Carro
+        public get minhaCor(){
+            return this.cor;
+        };
+
+        // Método para retornar o nome do Carro
+        public get meuNome(){
+            return this.nome;
+        };
+
+        // Método para retornar se o Carro está ligado ou desligado
+        public get estouLigado(){
+            return (this.motor.liga?'Sim':'Não');
+        };
+
+        // Método para retornar a potência do Carro
+        public get minhaPotencia(){
+            return this.motor.potenciaMotor;
+        };
+    };
+
+    export class CarroEsportivo extends Carro {
+        constructor(nome:string, cor:Cores){
+            super(nome, new Motores.Motor(6, 300, new Motores.Turbo(100)), cor);
+        };
+    };
+
+    export class CarroPopular extends Carro {
+        constructor(nome:string, cor:Cores){
+            super(nome, new Motores.Motor(3, 100), cor);
         };
     };
 
@@ -15,7 +68,7 @@ namespace Veiculos {
 
 namespace Motores {
 
-    class Turbo {
+    export class Turbo {
         private potencia:number;
 
         constructor(potencia:number){
@@ -56,3 +109,13 @@ namespace Motores {
     };
 
 };
+
+const carro1 = new Veiculos.CarroEsportivo('Ferrari', 5);
+const carro2 = new Veiculos.CarroPopular('Civic', 0);
+
+carro1.ligar();
+carro2.ligar();
+
+console.log(`Nome: ${carro1.meuNome} | Cor: ${carro1.minhaCor} | Potência: ${carro1.minhaPotencia} | Ligado: ${carro1.estouLigado}`);
+console.log('---------------------------------------------');
+console.log(`Nome: ${carro2.meuNome} | Cor: ${carro2.minhaCor} | Potência: ${carro2.minhaPotencia} | Ligado: ${carro2.estouLigado}`);
