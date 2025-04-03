a35b688a09f1b125b736b21656b03bba86874c45
Jardson Alan <jardsonalan132@gmail.com>
2025-04-03 18:40:49 -0300

    feat: adicionando conteúdo da aula sobre alterando elementos no DOM

---
+++ DevDojo/scripts/Alterando elementos no DOM/chagingElements.ts
@@ -0,0 +1,16 @@
+let divMain = <HTMLDivElement> document.getElementById('main')
+
+// Adiciona uma tag com informações dentro
+// divMain.innerHTML = '<span>DevDojo is freaking awesome</span>'
+
+// Adiciona um texto
+// divMain.textContent = 'DevDojo is freaking awesome'
+
+let newDiv = document.createElement('div')
+newDiv.textContent = 'DevDojo is freaking awesome'
+
+// Adiciona uma nova tag na última posição dos elementos filhos
+// divMain.appendChild(newDiv)
+
+// Adiciona uma nova tag na primeira posição dos elementos filhos
+divMain.insertBefore(newDiv, divMain.firstChild)
